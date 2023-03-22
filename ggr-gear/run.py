4d6e83b0cf33e9c65150af576082509651a3fa59
nicklbch <107702921+nicklbch@users.noreply.github.com>
2023-03-22 11:39:18 -0400

    Update run.py
    Updated to create optional inputs

--- ggr-gear/run.py
+++ ggr-gear/run.py
@@ -8,19 +8,40 @@ config = context.config
 
 #declare one required flag parameters as folder with dxm
 
-path1 = context.get_input('niftifileone')['location']['path']
-path2 = context.get_input('niftifiletwo')['location']['path']
-path3 = context.get_input('niftifilethree')['location']['path']
 
+if type(context.get_input('niftifilethree')) is dict:
 
-tempstr = "/flywheel/v0/output/data/"
-outputstr = "/flywheel/v0/output/recons/"
-workingstr = "/flywheel/v0/output/working/"
-os.system("mkdir " + tempstr)
-os.system("cp " + str(path1) + " " + str(path2) + " " + str(path3) + " /flywheel/v0/output/data")
-os.system("pwd")
-os.system("ls output/data")
-os.system("cd /flywheel/v0/output && python3 /opt/GGR-recon/preprocess.py")
-os.system("cd /flywheel/v0/output && python3 /opt/GGR-recon/recon.py --keep-negative-values --ggr -w 0.03")
-os.system("rm -rf " + tempstr)
-os.system("rm -rf " +  workingstr)
+    #print("Ran 3 inputs")
+    #print(three)
+    path1 = context.get_input('niftifileone')['location']['path']
+    path2 = context.get_input('niftifiletwo')['location']['path']
+    path3 = context.get_input('niftifilethree')['location']['path']
+    tempstr = "/flywheel/v0/output/data/"
+    outputstr = "/flywheel/v0/output/recons/"
+    workingstr = "/flywheel/v0/output/working/"
+    os.system("mkdir " + tempstr)
+    os.system("cp " + str(path1) + " " + str(path2) + " " + str(path3) + " /flywheel/v0/output/data")
+    os.system("pwd")
+    os.system("ls output/data")
+    os.system("cd /flywheel/v0/output && python3 /opt/GGR-recon/preprocess.py")
+    os.system("cd /flywheel/v0/output && python3 /opt/GGR-recon/recon.py --keep-negative-values --ggr -w 0.03")
+    os.system("rm -rf " + tempstr)
+    os.system("rm -rf " +  workingstr)
+
+else:
+
+    #print("Ran 2 inputs")
+    #print(two)
+    path1 = context.get_input('niftifileone')['location']['path']
+    path2 = context.get_input('niftifiletwo')['location']['path']
+    tempstr = "/flywheel/v0/output/data/"
+    outputstr = "/flywheel/v0/output/recons/"
+    workingstr = "/flywheel/v0/output/working/"
+    os.system("mkdir " + tempstr)
+    os.system("cp " + str(path1) + " " + str(path2) + " /flywheel/v0/output/data")
+    os.system("pwd")
+    os.system("ls output/data")
+    os.system("cd /flywheel/v0/output && python3 /opt/GGR-recon/preprocess.py")
+    os.system("cd /flywheel/v0/output && python3 /opt/GGR-recon/recon.py --keep-negative-values --ggr -w 0.03")
+    os.system("rm -rf " + tempstr)
+    os.system("rm -rf " +  workingstr)
